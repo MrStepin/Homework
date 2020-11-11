3ea788126360c88b7a0ffe4080e8cb4c2607c378
Stas <stanislavalex@mail.ru>
2020-11-11 17:03:29 +0300

    Add files via upload

---
+++ Commands/FlipCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Catel.MVVM;
+using System.Drawing;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConvolutionWpf.Commands
+{
+	public class FlipCommand : Command
+	{
+		private readonly Func<WriteableBitmap> _imageFactory;
+
+		public event Action<WriteableBitmap> OnImageChanged;
+
+		public FlipCommand(Func<WriteableBitmap> imageFactory)
+			: base(() => { })
+		{
+			_imageFactory = imageFactory;
+		}
+
+		public void ExecuteCommand()
+		{
+			var image = _imageFactory();
+			if (image == null)
+				return;
+
+			var pixels = new byte[image.PixelHeight * image.BackBufferStride];
+			image.CopyPixels(pixels, image.BackBufferStride, 0);
+
+
+			var imageRes = new WriteableBitmap(2 * image.PixelWidth, image.PixelHeight, image.DpiX, image.DpiY, image.Format, image.Palette);
+			var resultPixels = new byte[imageRes.PixelHeight * imageRes.BackBufferStride];
+
+			for (int i = 0; i < image.PixelHeight; i++)
+			{
+				for (int j = 0; j < image.PixelWidth; j++)
+				{
+					int indexOld = i * image.BackBufferStride + 4 * j;
+
+					int indexNew = i * imageRes.BackBufferStride + 4 * j;
+
+					for (int c = 0; c < 3; ++c)
+					{
+						resultPixels[indexNew + c] = pixels[indexOld + c];
+					}
+
+
+				}
+			}
+
+			for (int i = 0; i < image.PixelHeight; i++)
+			{
+				int k = 1;
+				for (int j = imageRes.PixelWidth -1; j > image.PixelWidth; j--)
+				{
+					int indexFlip = i * imageRes.BackBufferStride + 4 * j;
+
+					int index = i * image.BackBufferStride + 4 * (j + k - imageRes.PixelWidth);
+
+					for (int c = 0; c < 3; c++)
+					{
+						resultPixels[indexFlip + c] = pixels[index + c];
+					}
+					k += 2;
+				}
+
+			}
+
+					imageRes.WritePixels(new Int32Rect(0, 0, imageRes.PixelWidth, imageRes.PixelHeight), resultPixels, imageRes.BackBufferStride, 0);
+
+			OnImageChanged?.Invoke(imageRes);
+		}
+
+		protected override void Execute(object parameter, bool ignoreCanExecuteCheck)
+		{
+			ExecuteCommand();
+		}
+	}
+}
---
+++ Commands/NegateCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Catel.MVVM;
+
+namespace ConvolutionWpf.Commands
+{
+    public class NegateCommand : Command
+    {
+        private readonly Func<WriteableBitmap> _imageFactory;
+
+        public NegateCommand(Func<WriteableBitmap> imageFactory)
+            : base(() => { })
+        {
+            _imageFactory = imageFactory;
+        }
+
+        public void ExecuteCommand()
+        {
+            var image = _imageFactory();
+            if (image == null)
+                return;
+
+            var pixels = new byte[image.PixelHeight * image.BackBufferStride];
+            image.CopyPixels(pixels, image.BackBufferStride, 0);
+
+            var resultPixels = new byte[image.PixelHeight * image.BackBufferStride];
+
+            for (int i = 0; i < image.PixelWidth; ++i)
+            {
+                for (int j = 0; j < image.PixelHeight; ++j)
+                {
+                    int index = j * image.BackBufferStride + 4 * i;
+
+                    double red = pixels[index];
+                    double green = pixels[index + 1];
+                    double blue = pixels[index + 2];
+
+                    byte negate = (byte)(255 - red + 255 - green + 255 - blue); 
+
+                    for (int c = 0; c < 3; ++c)
+                    {
+                        resultPixels[index + c] = negate;
+                    }
+
+                    resultPixels[index + 3] = pixels[index + 3];
+                }
+            }
+
+
+            image.WritePixels(new Int32Rect(0, 0, image.PixelWidth, image.PixelHeight), resultPixels, image.BackBufferStride, 0);
+        }
+
+        protected override void Execute(object parameter, bool ignoreCanExecuteCheck)
+        {
+            ExecuteCommand();
+        }
+    }
+}
