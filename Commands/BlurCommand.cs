8864039ef22637e1cbb14a1e04d3605a301adec6
Stas <stanislavalex@mail.ru>
2020-11-14 17:56:15 +0300

    Add files via upload

--- Commands/BlurCommand.cs
+++ Commands/BlurCommand.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using Catel.MVVM;
@@ -32,25 +34,21 @@ namespace ConvolutionWpf.Commands
                 {
                     int indexOld = i * image.BackBufferStride + 4 * j;
 
-                    int KernelIndex5 = i * image.BackBufferStride + 4 * j;
-                    int KernelIndex1 = (i - 1) * image.BackBufferStride + 4 * (j - 1);
-                    int KernelIndex2 = (i - 1) * image.BackBufferStride + 4 * j;
-                    int KernelIndex3 = (i - 1) * image.BackBufferStride + 4 * (j + 1);
-                    int KernelIndex4 = i * image.BackBufferStride + 4 * (j - 1);
-                    int KernelIndex6 = i * image.BackBufferStride + 4 * (j + 1);
-                    int KernelIndex7 = (i + 1) * image.BackBufferStride + 4 * (j - 1);
-                    int KernelIndex8 = (i + 1) * image.BackBufferStride + 4 * j ;
-                    int KernelIndex9 = (i + 1) * image.BackBufferStride + 4 * (j + 1);
-
                     for (int c = 0; c < 3; ++c)
                     {
-                        resultPixels[indexOld + c] = (byte)((pixels[KernelIndex1 + c] * 1 + pixels[KernelIndex2 + c] * 1 + pixels[KernelIndex3 + c] * 1 +
-                                      pixels[KernelIndex4 + c] * 1 + pixels[KernelIndex5 + c] * 1 + pixels[KernelIndex6 + c] * 1 +
-                                      pixels[KernelIndex7 + c] * 1 + pixels[KernelIndex8 + c] * 1 + pixels[KernelIndex9 + c] * 1) / 9);
+                        int resultIndex = 0;
+
+                        for (int k = i - 1; k < i + 2; k++)
+                        {
+                            for (int m = j - 1; m < j + 2; m++)
+                            {
+                                int KernelIndex = (byte) (pixels[(k * image.BackBufferStride + 4 * m) + c] *1);
+                                resultIndex += KernelIndex;
+                            }
+                        }
+                        resultPixels[indexOld + c] = (byte)(resultIndex/ 9);
                     }
-                    resultPixels[indexOld + 3] = (byte)((pixels[KernelIndex1 + 3] * 1 + pixels[KernelIndex2 + 3] * 1 + pixels[KernelIndex3 + 3] * 1 +
-                                      pixels[KernelIndex4 + 3] * 1 + pixels[KernelIndex5 + 3] * 1 + pixels[KernelIndex6 + 3] * 1 +
-                                      pixels[KernelIndex7 + 3] * 1 + pixels[KernelIndex8 + 3] * 1 + pixels[KernelIndex9 + 3] * 1) / 9);
+
 
                 }
             }
