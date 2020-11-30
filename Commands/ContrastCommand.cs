c8364b81833e81b3145b26602ada1f1a1a870be9
Stas <stanislavalex@mail.ru>
2020-11-30 23:00:07 +0300

    Add files via upload

--- Commands/ContrastCommand.cs
+++ Commands/ContrastCommand.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Media.Imaging;
 using Catel.MVVM;
@@ -26,8 +28,88 @@ namespace ConvolutionWpf.Commands
 
             var resultPixels = new byte[image.PixelHeight * image.BackBufferStride];
 
-            //int index = j * image.BackBufferStride + 4 * i;
-            //todo
+            int[] sourceColors = Enumerable.Range(0, 256).ToArray();
+
+            Dictionary<int, int> cumulativeHistogram = new Dictionary<int, int>();
+
+            int pixelsCount = image.PixelWidth * image.PixelHeight;
+
+            foreach (byte color in sourceColors) 
+            {
+                int colorCount = 0;
+
+                for (int i = 0; i < image.PixelWidth; ++i)
+                {
+                    for (int j = 0; j < image.PixelHeight; ++j)
+                    {
+                        int index = j * image.BackBufferStride + i;
+
+                        if (pixels[index] == color)
+                            {
+                            colorCount += 1;
+                            }
+                    }
+                }
+
+                cumulativeHistogram.Add(color, colorCount);
+            }
+
+            List<int> low = new List<int>();
+            List<int> high = new List<int>();
+
+            int keyOfCumulativeHistogram = 0;
+            foreach (var value in cumulativeHistogram.Values)
+            {
+                if (value < 0.05 * pixelsCount)
+                {
+                    low.Add(keyOfCumulativeHistogram);
+                }
+                else if(value > 0.95 * pixelsCount)
+                {
+                    high.Add(keyOfCumulativeHistogram);
+                }
+                keyOfCumulativeHistogram += 1;
+            }
+
+            int minValue = 0;
+            int maxValue = 255;
+            byte resultByte = 0;
+
+            if (low.Count != 0)
+            {
+                minValue = low.Max();
+            }
+
+            if (high.Count != 0)
+            {
+                maxValue = high.Min();
+            }
+
+            for (int i = 0; i < image.PixelWidth; ++i)
+            {
+                for (int j = 0; j < image.PixelHeight; ++j)
+                {
+                    int index = j * image.BackBufferStride + 4 * i;
+
+                    if (pixels[index] <= minValue)
+                    {
+                        resultByte = 0;
+                    }
+                    else if (pixels[index] >= maxValue)
+                    {
+                        resultByte = 255;
+                    }
+                    else
+                    {
+                        resultByte = (byte)(pixels[index] - minValue / (maxValue - minValue));
+                    }
+
+                    for (int c = 0; c < 3; ++c)
+                    {
+                        resultPixels[index + c] = resultByte;
+                    }
+                }
+            }
 
             image.WritePixels(new Int32Rect(0, 0, image.PixelWidth, image.PixelHeight), resultPixels, image.BackBufferStride, 0);
         }
